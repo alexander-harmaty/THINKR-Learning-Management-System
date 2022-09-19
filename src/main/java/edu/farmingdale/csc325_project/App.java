bb72b9fceedee6a06e5616f63e538cab2ecc38f0
britgw@farmingdale.edu <skysn@137.125.129.211>
2022-09-18 21:47:46 -0400

    George initial push

--- src/main/java/edu/farmingdale/csc325_project/App.java
+++ src/main/java/edu/farmingdale/csc325_project/App.java
@@ -32,7 +32,7 @@ public class App extends Application {
     }
 
     public static void main(String[] args) {
-        //System.out.println("George push");
+        System.out.println("George push");
         //System.out.println("Trinity push");
         System.out.println("Forhad push");
         
