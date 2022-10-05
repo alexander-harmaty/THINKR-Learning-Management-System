7a085629e87b34bbc54860973b36184ec658390f
alexander-harmaty <92049896+alexander-harmaty@users.noreply.github.com>
2022-10-05 18:34:52 -0400

    JDBC dependency fix

--- src/main/java/edu/farmingdale/csc325_project/DBConnection.java
+++ src/main/java/edu/farmingdale/csc325_project/DBConnection.java
@@ -6,7 +6,6 @@ package edu.farmingdale.csc325_project;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import javax.swing.JOptionPane;
 
 /**
  * This class simplifies the connection to the database with the connectDB() method.
@@ -16,18 +15,17 @@ import javax.swing.JOptionPane;
  * @author AlexH
  */
 public class DBConnection {
-    public static Connection connectDB() {
-        //String user = "pportal";
-        //String pass = "admin";
-        try{
-            //Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver"); 
-            //Connection con = DriverManager.getConnection("jdbc:sqlserver://24.45.36.120:1433;"
-            //            + "databaseName=PatientPortal;encrypt=true;trustServerCertificate=true;", user, pass);
-            //return con;
-        }
-        catch (Exception e) {
-            //javax.swing.JOptionPane.showMessageDialog(null, e);
-            return null;
-        }
-    }
+//    public static Connection connectDB() {
+//        String user = "pportal";
+//        String pass = "admin";
+//        try{
+//            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver"); 
+//            Connection con = DriverManager.getConnection("jdbc:sqlserver://24.45.36.120:1433;"
+//                        + "databaseName=PatientPortal;encrypt=true;trustServerCertificate=true;", user, pass);
+//            return con;
+//        }
+//        catch (Exception e) {
+//            return null;
+//        }
+//    }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module edu.farmingdale.csc325_project {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.base;
+    requires java.sql;
 
     opens edu.farmingdale.csc325_project to javafx.fxml;
     exports edu.farmingdale.csc325_project;
