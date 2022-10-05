002b546e979876cb85f8216e87754c91a31ebff6
alexander-harmaty <92049896+alexander-harmaty@users.noreply.github.com>
2022-10-05 14:33:40 -0400

    Updated FXML for Login & Register

--- src/main/java/edu/farmingdale/csc325_project/LoginController.java
+++ src/main/java/edu/farmingdale/csc325_project/LoginController.java
@@ -4,9 +4,13 @@
  */
 package edu.farmingdale.csc325_project;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.Initializable;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 
 /**
  * FXML Controller class
@@ -14,13 +18,32 @@ import javafx.fxml.Initializable;
  * @author AlexH
  */
 public class LoginController implements Initializable {
-
+    
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
+    }
+    
+    @FXML
+    private Button button_login, button_register;
+
+    @FXML
+    private TextField textField_email, textField_password;
+    
+    public void handleButton_register() throws IOException {
+        App.setRoot("Register");
+    }
+    
+    public void handleButton_login() throws IOException {
+        //connect to database
+        //compare textFields to logins table
+        //if an email and password match, 
+            //read account type, then switch to appropriate view
+            
+        //App.setRoot("");
+    }
     
 }
--- src/main/java/edu/farmingdale/csc325_project/RegisterController.java
+++ src/main/java/edu/farmingdale/csc325_project/RegisterController.java
@@ -4,9 +4,14 @@
  */
 package edu.farmingdale.csc325_project;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
 
 /**
  * FXML Controller class
@@ -23,4 +28,25 @@ public class RegisterController implements Initializable {
         // TODO
     }    
     
+    @FXML
+    private TextField textField_email1, textField_email2, textField_password1, 
+            textField_password2, textField_firstName, textField_lastName;
+    
+    @FXML
+    private DatePicker datePicker_DOB;
+    
+    @FXML
+    private TextField textField_accountType;
+    
+    public void handleButton_back() throws IOException{
+        App.setRoot("Login");
+    }
+    
+    public void handleButton_register() throws IOException{
+        //connect to database
+        //create records in logins and users table
+        //setRoot to login screen, & autofill fields
+        
+        App.setRoot("Login");
+    }
 }
