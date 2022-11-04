e7fb2b0b6e955f6354a3fc8d1bb381b2be355f79
alexander-harmaty <92049896+alexander-harmaty@users.noreply.github.com>
2022-11-04 17:11:51 -0400

    MaterialFX

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,7 @@ module edu.farmingdale.csc325_project {
     requires javafx.fxml;
     requires java.base;
     requires java.sql;
+    requires MaterialFX;
 
     opens edu.farmingdale.csc325_project to javafx.fxml;
     exports edu.farmingdale.csc325_project;
