4190257bb6dbf56cc0eff54becce7cb183c7157d
darbt <darbt3@farmingdale.edu>
2022-11-16 23:08:03 -0500

    professor fix

--- src/main/java/edu/farmingdale/csc325_project/ProfessorViewController.java
+++ src/main/java/edu/farmingdale/csc325_project/ProfessorViewController.java
@@ -1,10 +1,5 @@
 package edu.farmingdale.csc325_project;
 
-
-import java.net.URL;
-import java.util.ResourceBundle;
-import javafx.event.ActionEvent;
-import javafx.fxml.Initializable;
 import io.github.palexdev.materialfx.controls.MFXButton;
 import javafx.fxml.FXML;
 
