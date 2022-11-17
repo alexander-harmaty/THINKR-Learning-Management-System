060f48610cffd5f55728e12871586bfa8e298e0a
darbt <darbt3@farmingdale.edu>
2022-11-17 13:27:21 -0500

    update prof

--- src/main/java/edu/farmingdale/csc325_project/ProfessorViewController.java
+++ src/main/java/edu/farmingdale/csc325_project/ProfessorViewController.java
@@ -1,20 +1,41 @@
 package edu.farmingdale.csc325_project;
 
 import io.github.palexdev.materialfx.controls.MFXButton;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 
 public class ProfessorViewController {
 
     @FXML
-    private MFXButton calander_button;
+    private MFXButton button_calendar;
 
     @FXML
-    private MFXButton courses_button;
+    private MFXButton button_courses;
 
     @FXML
-    private MFXButton home_button;
+    private MFXButton button_home;
 
     @FXML
-    private MFXButton setting_button;
+    private MFXButton button_settings;
+
+    @FXML
+    void handleButton_calendar(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleButton_courses(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleButton_home(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleButton_settings(ActionEvent event) {
+
+    }
 
 }
