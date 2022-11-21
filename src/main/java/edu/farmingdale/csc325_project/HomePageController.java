1dd5404cf2eb2cb6735abcf882641c9b9b287868
alexander-harmaty <92049896+alexander-harmaty@users.noreply.github.com>
2022-11-21 13:19:42 -0500

    latest

--- src/main/java/edu/farmingdale/csc325_project/HomePageController.java
+++ src/main/java/edu/farmingdale/csc325_project/HomePageController.java
@@ -4,6 +4,7 @@ import io.github.palexdev.materialfx.controls.MFXButton;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -18,12 +19,28 @@ public class HomePageController implements Initializable{
     
     @FXML
     private VBox VBox_navButtons;
-
+    
     @FXML
-    private MFXButton button_home, button_settings, 
-            button_calendar, button_courses, button_grades, 
-            button_registrar, button_accounts;
-
+    private MFXButton button_home = new MFXButton("Home");
+    
+    @FXML
+    private MFXButton button_courses = new MFXButton("Courses");
+    
+    @FXML
+    private MFXButton button_grades = new MFXButton("Grades");
+    
+    @FXML
+    private MFXButton button_calendar = new MFXButton("Calendar");
+    
+    @FXML
+    private MFXButton button_registrar = new MFXButton("Registrar");
+    
+    @FXML
+    private MFXButton button_accounts = new MFXButton("Accounts");
+    
+    @FXML
+    private MFXButton button_settings = new MFXButton("Settings");
+    
     @FXML
     void handleButton_home(ActionEvent event) {
 
@@ -62,58 +79,53 @@ public class HomePageController implements Initializable{
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         VBox_navButtons.getChildren().clear();
+        
+        List<MFXButton> buttons = new ArrayList<>();
+        
         switch(App.currentUser.type) {
             case "STUDENT":
                 
-                button_home = new MFXButton("Home");
-                button_courses = new MFXButton("Courses");
-                button_grades = new MFXButton("Grades");
-                button_calendar = new MFXButton("Calendar");
-                button_registrar = new MFXButton("Registrar");
-                button_settings = new MFXButton("Settings");
-                
-                MFXButton buttons[] = {button_home, button_courses, button_grades, button_calendar, button_registrar, button_settings};
-                
-                for (MFXButton button : buttons) {
-                    Font font = new Font("System", 26);
-                    button.setFont(font);
-                    button.setPrefHeight(50);
-                    button.setMaxWidth(Double.MAX_VALUE);
-                    button.setMaxHeight(50);
-                    VBox_navButtons.getChildren().add(button);
-                }
+                buttons.add(button_home);
+                buttons.add(button_courses);
+                buttons.add(button_grades);
+                buttons.add(button_calendar);
+                buttons.add(button_registrar);
+                buttons.add(button_settings);
                 
                 break;
 
             case "PROFESSOR":
                 
-                button_home = new MFXButton("Home");
-                button_courses = new MFXButton("Courses");
-                button_grades = new MFXButton("Grades");
-                button_calendar = new MFXButton("Calendar");
-                button_settings = new MFXButton("Settings");
-                
-                VBox_navButtons.getChildren().addAll(
-                        button_home, button_courses, button_grades,
-                        button_calendar, button_settings
-                );
+                buttons.add(button_home);
+                buttons.add(button_courses);
+                buttons.add(button_grades);
+                buttons.add(button_calendar);
+                buttons.add(button_settings);
                 
                 break;
+                
             case "ADMIN":
                 
-                button_home = new MFXButton("Home");
-                button_registrar = new MFXButton("Registrar");
-                button_accounts = new MFXButton("Accounts");
-                button_settings = new MFXButton("Settings");
-                
-                VBox_navButtons.getChildren().addAll(
-                        button_home, button_registrar, 
-                        button_accounts, button_settings
-                );
+                buttons.add(button_home);
+                buttons.add(button_registrar);
+                buttons.add(button_settings);
+                buttons.add(button_settings);
                 
                 break;
+                
             default:
+                
                 break;
+                
+        }
+        
+        for (MFXButton button : buttons) {
+            Font font = new Font("System", 26);
+            button.setFont(font);
+            button.setPrefHeight(50);
+            button.setMaxWidth(Double.MAX_VALUE);
+            button.setMaxHeight(50);
+            VBox_navButtons.getChildren().add(button);
         }
         
     }
