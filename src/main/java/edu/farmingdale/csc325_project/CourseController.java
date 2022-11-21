0d78287d4facebc3e10f5956dae686872884e8d9
darbt <darbt3@farmingdale.edu>
2022-11-21 15:34:43 -0500

    latest

---
+++ src/main/java/edu/farmingdale/csc325_project/CourseController.java
@@ -0,0 +1,36 @@
+package edu.farmingdale.csc325_project;
+
+import io.github.palexdev.materialfx.controls.MFXTableView;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import org.controlsfx.control.action.ActionUtils;
+
+public class CourseController extends HomePageController implements Initializable{
+    
+
+    @FXML
+    private VBox VBox_navBar;
+
+    @FXML
+    private VBox VBox_navButtons;
+
+    @FXML
+    private Label label_classTitle;
+
+    @FXML
+    private MFXTableView<?> tableView_Announce;
+
+    @FXML
+    private MFXTableView<?> tableView_Course;
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        updateMenu();
+    
+   
+    }
+}
