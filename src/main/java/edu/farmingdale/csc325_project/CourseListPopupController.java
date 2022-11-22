ba475595fde2a48b293278248bfdbf47210ae54a
darbt <darbt3@farmingdale.edu>
2022-11-22 10:54:54 -0500

    Update CourseListPopupController.java

--- src/main/java/edu/farmingdale/csc325_project/CourseListPopupController.java
+++ src/main/java/edu/farmingdale/csc325_project/CourseListPopupController.java
@@ -1,11 +1,28 @@
 package edu.farmingdale.csc325_project;
 
+import com.google.api.core.ApiFuture;
+import com.google.cloud.firestore.DocumentReference;
+import com.google.cloud.firestore.QueryDocumentSnapshot;
+import com.google.cloud.firestore.QuerySnapshot;
+import com.google.cloud.firestore.WriteResult;
+import com.google.firestore.v1.Document;
 import io.github.palexdev.materialfx.controls.MFXTableView;
+import java.net.URL;
+import java.util.ResourceBundle;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 
-public class CourseListPopupController {
+public class CourseListPopupController implements Initializable{
 
     @FXML
     private MFXTableView<?> tableView_popup;
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        
+      
+       
+       
+    }
 
 }
