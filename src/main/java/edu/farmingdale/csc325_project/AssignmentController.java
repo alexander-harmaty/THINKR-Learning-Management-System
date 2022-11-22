92ae7ddc5e17fd24a4c5e6c11847bb327eecad3a
Forhad <112634349+fahifh@users.noreply.github.com>
2022-11-22 13:06:24 -0500

    Assignment

---
+++ src/main/java/edu/farmingdale/csc325_project/AssignmentController.java
@@ -0,0 +1,27 @@
+package edu.farmingdale.csc325_project;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author forha
+ */
+public class AssignmentController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
