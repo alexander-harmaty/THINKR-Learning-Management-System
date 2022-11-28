8160315c57c9f3f0b678945188bee5e7d95010d0
darbt <darbt3@farmingdale.edu>
2022-11-28 12:30:57 -0500

    Create Assignment.java

---
+++ src/main/java/edu/farmingdale/csc325_project/Assignment.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package edu.farmingdale.csc325_project;
+
+import com.google.cloud.firestore.DocumentReference;
+import com.google.cloud.firestore.QueryDocumentSnapshot;
+import java.io.IOException;
+import java.util.List;
+
+/**
+ *
+ * @author trintydarbouze
+ */
+public class Assignment {
+    
+    protected int ID; 
+    protected String assignDate; 
+    protected String details; 
+    protected String grade; 
+    protected String professor; 
+    protected List<String> students;
+    
+    
+    
+    
+}
