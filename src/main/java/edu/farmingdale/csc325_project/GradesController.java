1c9ac6f81d962f9229e747590996d862ad1541d0
George <britgw@farmingdale.edu>
2022-11-30 17:40:57 -0500

    grades

--- src/main/java/edu/farmingdale/csc325_project/GradesController.java
+++ src/main/java/edu/farmingdale/csc325_project/GradesController.java
@@ -1,12 +1,21 @@
 package edu.farmingdale.csc325_project;
 
+import com.google.api.core.ApiFuture;
+import com.google.cloud.firestore.QueryDocumentSnapshot;
+import com.google.cloud.firestore.QuerySnapshot;
+import java.net.URL;
+import java.util.List;
+import java.util.ResourceBundle;
+import java.util.concurrent.ExecutionException;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
-public class GradesController {
+public class GradesController extends HomePageController implements Initializable {
 
     @FXML
     private HBox HBox_t;
@@ -18,18 +27,29 @@ public class GradesController {
     private VBox VBox_navButtons;
 
     @FXML
-    private TableColumn<?, ?> tableColumn_assignment;
+    private TableColumn<Assignment, String> tableColumn_assignment;
 
     @FXML
-    private TableColumn<?, ?> tableColumn_course;
+    private TableColumn<Assignment, String> tableColumn_course;
 
     @FXML
-    private TableColumn<?, ?> tableColumn_dueDate;
+    private TableColumn<Assignment, String> tableColumn_dueDate;
 
     @FXML
-    private TableColumn<?, ?> tableColumn_grade;
+    private TableColumn<Assignment, Integer> tableColumn_grade;
 
     @FXML
-    private TableView<?> tableView_grades;
+    private TableView<Assignment> tableView_grades;
 
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+
+        tableColumn_dueDate.setCellValueFactory(new PropertyValueFactory<>("dueDate"));
+        tableColumn_course.setCellValueFactory(new PropertyValueFactory<>("course"));
+        tableColumn_grade.setCellValueFactory(new PropertyValueFactory<>("grade"));
+
+        readSubmissionsIntoTable();
+    }
+    
+    
 }
