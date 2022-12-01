183d9b552259ddd92c1aa020f062d7fe3f6a8e26
Forhad <112634349+fahifh@users.noreply.github.com>
2022-12-01 14:02:31 -0500

    fixed up Admin Controller

--- src/main/java/edu/farmingdale/csc325_project/AdminController.java
+++ src/main/java/edu/farmingdale/csc325_project/AdminController.java
@@ -2,11 +2,20 @@ package edu.farmingdale.csc325_project;
 
 import io.github.palexdev.materialfx.controls.MFXButton;
 import io.github.palexdev.materialfx.controls.MFXTextField;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.VBox;
 
-public class AdminController {
+public class AdminController extends HomePageController implements Initializable {
 
     @FXML
     private VBox VBox_navBar;
@@ -24,19 +33,22 @@ public class AdminController {
     private MFXButton button_update;
 
     @FXML
-    private TableColumn<?, ?> tableColumn_dob;
+    private TableColumn<User,String> tableColumn_dob;
 
     @FXML
-    private TableColumn<?, ?> tableColumn_email;
+    private TableColumn<User,String> tableColumn_email;
 
     @FXML
-    private TableColumn<?, ?> tableColumn_firstName;
+    private TableColumn<User,String> tableColumn_firstName;
 
     @FXML
-    private TableColumn<?, ?> tableColumn_lastName;
+    private TableColumn<User,String> tableColumn_lastName;
 
     @FXML
-    private TableColumn<?, ?> tableColumn_type;
+    private TableColumn<User,String> tableColumn_type;
+
+    @FXML
+    private TableView<User> tableView_userTable;
 
     @FXML
     private MFXTextField textField_dob;
@@ -53,4 +65,30 @@ public class AdminController {
     @FXML
     private MFXTextField textField_type;
 
+    private ObservableList<User> listOfUsers = FXCollections.observableArrayList();
+
+    public ObservableList<User> getListOfUsers() {
+        return listOfUsers;
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        tableColumn_dob.setCellValueFactory(new PropertyValueFactory<>("DOB"));
+        tableColumn_email.setCellValueFactory(new PropertyValueFactory<>("Email"));
+        tableColumn_firstName.setCellValueFactory(new PropertyValueFactory<>("First Name"));
+        tableColumn_lastName.setCellValueFactory(new PropertyValueFactory<>("Last Name"));
+        tableColumn_type.setCellValueFactory(new PropertyValueFactory<>("Type"));
+
+        tableView_userTable.setOnMousePressed(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                textField_dob.setText(tableView_userTable.getSelectionModel().getSelectedItem().getDOB());
+                textField_email.setText(tableView_userTable.getSelectionModel().getSelectedItem().getEmail());
+                textField_firstName.setText(tableView_userTable.getSelectionModel().getSelectedItem().getFirstName());
+                textField_lastName.setText(tableView_userTable.getSelectionModel().getSelectedItem().getLastName());
+                textField_type.setText(tableView_userTable.getSelectionModel().getSelectedItem().getType());
+            }
+        });
+
+    }
 }
