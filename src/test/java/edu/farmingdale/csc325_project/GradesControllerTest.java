d4145f7fdc8e052598a03e762b622d806f73d9f0
fahifh <112634349+fahifh@users.noreply.github.com>
2022-12-08 03:10:07 -0500

    test cases created

---
+++ src/test/java/edu/farmingdale/csc325_project/GradesControllerTest.java
@@ -0,0 +1,96 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+package edu.farmingdale.csc325_project;
+
+import java.net.URL;
+import java.util.List;
+import java.util.ResourceBundle;
+import javafx.collections.ObservableList;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author forha
+ */
+public class GradesControllerTest {
+
+    @org.junit.jupiter.api.Test
+    public void testClassAvgCalculator() {
+        System.out.println("classAvgCalculator");
+        List<Integer> grades = null;
+        GradesController instance = new GradesController();
+        double expResult = 0.0;
+        double result = instance.classAvgCalculator(grades);
+        assertEquals(expResult, result, 0.0);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+    @org.junit.jupiter.api.Test
+    public void testClassAvgCalculator1() {
+        System.out.println("classAvgCalculator");
+        List<Integer> grades = null;
+        GradesController instance = new GradesController();
+        double expResult = 0.0;
+        double result = instance.classAvgCalculator(grades);
+        assertEquals(expResult, result, 0.0);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+    @org.junit.jupiter.api.Test
+    public void testClassAvgCalculato2r() {
+        System.out.println("classAvgCalculator");
+        List<Integer> grades = null;
+        GradesController instance = new GradesController();
+        double expResult = 0.0;
+        double result = instance.classAvgCalculator(grades);
+        assertEquals(expResult, result, 0.0);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+    @org.junit.jupiter.api.Test
+    public void testClassAvgCalculator3() {
+        System.out.println("classAvgCalculator");
+        List<Integer> grades = null;
+        GradesController instance = new GradesController();
+        double expResult = 0.0;
+        double result = instance.classAvgCalculator(grades);
+        assertEquals(expResult, result, 0.0);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+    @org.junit.jupiter.api.Test
+    public void testClassAvgCalculator4() {
+        System.out.println("classAvgCalculator");
+        List<Integer> grades = null;
+        GradesController instance = new GradesController();
+        double expResult = 0.0;
+        double result = instance.classAvgCalculator(grades);
+        assertEquals(expResult, result, 0.0);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+    @org.junit.jupiter.api.Test
+    public void testClassAvgCalculator5() {
+        System.out.println("classAvgCalculator");
+        List<Integer> grades = null;
+        GradesController instance = new GradesController();
+        double expResult = 0.0;
+        double result = instance.classAvgCalculator(grades);
+        assertEquals(expResult, result, 0.0);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+}
